6c00ce8c6d580a9fe7dda7b48862da09c9be9194
rustamovjavohir <rustamovj366@gmail.com>
2023-03-10 17:27:36 +0500

    changes for clean code

--- fixtures/dumpdata_scr.py
+++ fixtures/dumpdata_scr.py
@@ -1,5 +1,6 @@
 import subprocess
-from config.settings import BASE_DIR
+from config.settings import BASE_DIR, S_TOKEN
+from telegram import Bot
 
 
 def dumpDataScript():
@@ -12,6 +13,9 @@ def dumpDataScript():
     # Run the command and wait for it to finish
     subprocess.run(command, cwd=working_directory, shell=True)
     subprocess.run(command, shell=True)
+
+    bot = Bot(token=S_TOKEN)
+    bot.send_document(chat_id=779890968, document=open('data.json', 'rb'))
 
 
 if __name__ == '__main__':
