c8e4159694d2719571950b1b0d0ce4a4b5a33cb4
rustamovjavohir <rustamovj366@gmail.com>
2023-04-25 14:56:38 +0500

    change api structura

--- static/intranet/js/webcam.js
+++ static/intranet/js/webcam.js
@@ -45,50 +45,64 @@ function open_webcam() {
 }
 
 
-function take_snapshot() {
-
-    // take snapshot and get image data
-    Webcam.snap(function (data_uri) {
-
-        Webcam.snap((data_uri) => {
-            const canvas = document.getElementById('canvas');
-            const context = canvas.getContext('2d');
-            const image = new Image();
-            let dateTime = getDateTime()
-            image.onload = () => {
-                // context.drawImage(image, 0, 0, 640, 480);
-                context.drawImage(image, 0, 0, 810, 520);
-                context.font = "20px Arial"
-
-                context.fillText(dateTime, 10, 30);
-                context.fillText(user, 10, 50);
-                const imageData = canvas.toDataURL('image/png');
-                fetch('/api/save-image/', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRFToken': getCSRFToken()
-                    },
-                    body: JSON.stringify({
-                        imageData: imageData
-                        // imageData: "salom"
-                    })
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        console.log(data);
-                    });
-            };
-            image.src = data_uri;
-        });
-    });
-}
-
-
 function getDateTime() {
     let now = new Date();
     let date = now.getDate() + '/' + (now.getMonth() + 1) + '/' + now.getFullYear();
     let time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
     let dateTime = time + '  ' + date;
     return dateTime
+}
+
+function take_snapshot() {
+    let user_val = $('#user_select').val();
+    if (user_val === '-1')
+        Swal.fire(
+            'Foydalanuvchi tanlanmadi',
+            'Iltimos foydalanuvchini tanlang',
+            'error'
+        )
+    else {
+        Webcam.snap(function (data_uri) {
+            Webcam.snap((data_uri) => {
+                const canvas = document.getElementById('canvas');
+                const context = canvas.getContext('2d');
+                const image = new Image();
+                let dateTime = getDateTime()
+                image.onload = () => {
+                    context.drawImage(image, 0, 0, 810, 520);
+                    context.font = "20px Arial"
+
+                    context.fillText(dateTime, 10, 30);
+                    context.fillText(user, 10, 60);
+                    context.fillText(user_val, 10, 90);
+                    const imageData = canvas.toDataURL('image/png');
+                    fetch('/api/save-image/', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRFToken': getCSRFToken()
+                        },
+                        body: JSON.stringify({
+                            imageData: imageData
+                            // imageData: "salom"
+                        })
+                    })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            console.log(data);
+                        });
+                };
+                image.src = data_uri;
+            });
+        });
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 2000,
+            width: 400,
+        })
+
+    }
 }
