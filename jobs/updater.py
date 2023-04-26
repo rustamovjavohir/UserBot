91708addc2b49558c2188d1637be95d43ba295a0
rustamovjavohir <rustamovj366@gmail.com>
2023-04-26 14:19:33 +0500

    add style

--- jobs/updater.py
+++ jobs/updater.py
@@ -7,7 +7,7 @@ def startUpdater():
     sss = BackgroundScheduler({'apscheduler.timezone': 'Asia/Tashkent'})
     sss.add_job(updateTotal, "interval", seconds=300)
     sss.add_job(updateTotal, "cron", hour=23, minute=00)
-    sss.add_job(addSalary, "cron", day=26, hour=14, minute=20)
+    sss.add_job(addSalary, "cron", day=26, hour=14, minute=30)
     # sss.add_job(notificationSalary, "cron", day=29, hour=11, minute=11)
     # sss.add_job(ttime, "cron", hour=23, minute=50)
     # sss.add_job(hero_month, "cron", month="1-12", day=1, hour=5, minute=30)
