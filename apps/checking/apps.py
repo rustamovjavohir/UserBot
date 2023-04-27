6956dc328b2464b3136d42a8a03f87b8cd3c77d2
rustamovjavohir <rustamovj366@gmail.com>
2023-04-27 15:11:17 +0500

    add worker check_in and check_out model

--- apps/checking/apps.py
+++ apps/checking/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class CheckingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.checking'
+    verbose_name = "Проверка"
 
     def ready(self):
         import apps.checking.signals
