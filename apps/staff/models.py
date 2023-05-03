a56d5037a1d72363606ea5331bd3ceb2e2d223cd
rustamovjavohir <rustamovj366@gmail.com>
2023-05-03 10:49:30 +0500

    add created_at

--- apps/staff/models.py
+++ apps/staff/models.py
@@ -110,6 +110,7 @@ class Workers(models.Model):
     start_work = models.TimeField(default=datetime.datetime.strptime("09:00:00", '%H:%M:%S').time(),
                                   verbose_name="Начало работы")
     is_deleted = models.BooleanField(default=False, verbose_name="Удален")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
 
     def __str__(self):
         return self.full_name
