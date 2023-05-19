22cddf4833b14f47f176bbcf8483c59199233cd3
rustamovjavohir <rustamovj366@gmail.com>
2023-05-19 17:26:03 +0500

    add comment for checkin

--- apps/checking/admin.py
+++ apps/checking/admin.py
@@ -25,6 +25,8 @@ class AdminTimekeeping(admin.ModelAdmin):
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
         return qs.filter(is_deleted=False)
 
     def worker_department(self, obj):
