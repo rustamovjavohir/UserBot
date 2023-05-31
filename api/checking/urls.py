8acb9531fc48680ce20ebab13ebefd7af0982e9e
rustamovjavohir <rustamovj366@gmail.com>
2023-05-31 12:11:46 +0500

    add admin permission

--- api/checking/urls.py
+++ api/checking/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('save-image/', SaveImage.as_view(), name='save-image'),
     path('ip/', getIpAddress, name='get-ip'),
     path('timekeeping/profile', UserTimekeepingView.as_view(), name='timekeeping'),
-    path('timekeeping/profile/<int:pk>', WorkerTimekeepingView.as_view(), name='profile-timekeeping'),
+    path('timekeeping/workers/<int:pk>', WorkerTimekeepingView.as_view(), name='profile-timekeeping'),
     path('timekeeping/workers', WorkersDailyTimekeepingView.as_view(), name='workers-timekeeping'),
     path('timekeeping/set-time', SetTimekeepingView.as_view(), name='set-timekeeping'),
 
