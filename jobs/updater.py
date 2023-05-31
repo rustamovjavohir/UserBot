4f48c46a2637cfa067f2cdaefb01d3ebcdc43969
rustamovjavohir <rustamovj366@gmail.com>
2023-05-31 11:19:00 +0500

    change jobs (setCheckOut) time

--- jobs/updater.py
+++ jobs/updater.py
@@ -8,7 +8,7 @@ def startUpdater():
     sss.add_job(updateTotal, "interval", seconds=300)
     sss.add_job(updateTotal, "cron", hour=23, minute=00)
     sss.add_job(addSalary, "cron", day=25, hour=1, minute=30)
-    sss.add_job(setCheckOut, "cron", hour=15, minute=41)
+    sss.add_job(setCheckOut, "cron", hour=23, minute=55)
     # sss.add_job(ttime, "cron", hour=23, minute=50)
     # sss.add_job(hero_month, "cron", month="1-12", day=1, hour=5, minute=30)
     sss.start()
