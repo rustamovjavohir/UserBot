ecb742429c09655bf4d6a2409c61911a98d6aa3c
rustamovjavohir <rustamovj366@gmail.com>
2023-06-02 18:04:17 +0500

    add count_on_page

--- api/checking/paginations/paginations.py
+++ api/checking/paginations/paginations.py
@@ -1,4 +1,7 @@
+from collections import OrderedDict
+
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class TimekeepingPagination(PageNumberPagination):
@@ -12,3 +15,13 @@ class WorkerPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('total_pages', self.page.paginator.num_pages),
+            ('current_page', self.page.number),
+            ('count_on_page', self.page_size),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
