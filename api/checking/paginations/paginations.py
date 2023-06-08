012615b831b981d929e339fdb1d525b2b62f9d4f
rustamovjavohir <rustamovj366@gmail.com>
2023-06-08 14:07:10 +0500

    add page count

--- api/checking/paginations/paginations.py
+++ api/checking/paginations/paginations.py
@@ -9,6 +9,17 @@ class TimekeepingPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('total_pages', self.page.paginator.num_pages),
+            ('current_page', self.page.number),
+            ('count_on_page', self.page_size),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
+
 
 class WorkerPagination(PageNumberPagination):
     page_size = 10
