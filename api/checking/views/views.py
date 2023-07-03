c48acd699387fe8de0db86ebfa0a9ae588c4e649
rustamovjavohir <rustamovj366@gmail.com>
2023-07-03 13:49:43 +0500

    add 2 comment to check in

--- api/checking/views/views.py
+++ api/checking/views/views.py
@@ -149,7 +149,7 @@ class SetTimekeepingView(GenericAPIView):
             worker_time.setCheckInOrOut()
         if comment:
             old_comment = worker_time.comment if worker_time.comment else ""
-            worker_time.comment = old_comment + ", " + comment
+            worker_time.comment = old_comment + " " + comment + ","
             worker_time.save()
         serializer = self.get_serializer(worker_time)
         response = OrderedDict([
