c035501ca480eebc740e4dec52b04e54b55ae18a
rustamovjavohir <rustamovj366@gmail.com>
2023-07-07 15:48:57 +0500

    add user display in workers admin

--- api/checking/serializers/serializers.py
+++ api/checking/serializers/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+import locale
 from apps.checking.models import Timekeeping
 
 
@@ -10,11 +10,17 @@ class CheckingSerializer(serializers.Serializer):
 class TimekeepingSerializer(serializers.ModelSerializer):
     worker = serializers.CharField(source='worker.full_name', read_only=True)
     action = serializers.ChoiceField(choices=Timekeeping.ActionChoices, read_only=True)
+    date = serializers.SerializerMethodField(source='get_date')
 
     class Meta:
         model = Timekeeping
         fields = ['id', 'worker', 'date', 'check_in', 'check_out', 'comment', 'action']
         read_only_fields = ['worker', 'date', 'check_in', 'check_out']
+
+    def get_date(self, obj):
+        locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+        return obj.date.strftime('%a %d-%m-%Y')
+        # return obj.date.strftime(u'%a %d-%m-%Y')
 
 
 class TimekeepingDetailSerializer(serializers.ModelSerializer):
