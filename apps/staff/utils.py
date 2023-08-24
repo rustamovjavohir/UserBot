1503e90e933f3f805468d939449918dc61706ba6
rustamovjavohir <rustamovj366@gmail.com>
2023-08-24 12:36:44 +0500

    send boss permission action

--- apps/staff/utils.py
+++ apps/staff/utils.py
@@ -133,7 +133,7 @@ def getAvansText(name, req, month, salary, money, balance):
     text += f"<strong>Avans miqdori:</strong> {'{:,}'.format(money)} So`m\n" \
             f"<strong>Balans:</strong> {'{:,}'.format(balance)} So`m\n"
 
-    if balance <= salary * 0.3:
+    if balance <= salary * 0.3 or money >= salary * 0.7:
         text += f"\n<strong>⚠️Diqqat!</strong> Oylikning 70% dan ortiq miqdorda avans berilmoqda!\n"
     return text
 
@@ -350,7 +350,8 @@ def applyAvans(update: Update, context: CallbackContext, worker_id=None):
                                                   department=Workers.objects.get(
                                                       telegram_id=worker_id).department).first()
                     accept_button = acceptInlineButton(req.id)
-                    if month == datetime.now().month and obj.ostatok_1 + money <= obj.itog_1 * 0.3:
+                    if month == datetime.now().month and (obj.ostatok_1 <= obj.itog_1 * 0.3
+                                                          or money >= obj.itog_1 * 0.7):
                         accept_button = acceptInlineButton2(req.id)
                     context.bot.send_message(chat_id=boss.telegram_id, text=text, parse_mode="html",
                                              reply_markup=accept_button)
