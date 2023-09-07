6d1c2e01fae484dfe550181ec2fc7c7836a391c7
rustamovjavohir <rustamovj366@gmail.com>
2023-09-07 14:53:51 +0500

    remove automatic request celery

--- apps/staff/tasks.py
+++ apps/staff/tasks.py
@@ -1,55 +1,55 @@
-import json
-
-from celery import shared_task
-from django_celery_beat.models import PeriodicTask, CrontabSchedule, IntervalSchedule
-from telegram import Bot
-
-from apps.staff.models import Request_price
-from config import settings
-
-bot = Bot(token=settings.S_TOKEN)
-
-
-@shared_task
-def send_message(message):
-    bot.send_message(chat_id=settings.ADMIN_ID, text=message)
-    return message
-
-
-@shared_task
-def auto_del_request(message_id, chat_id, request_id):
-    try:
-        Request_price.objects.filter(id=request_id, status=Request_price.Status.PENDING, answer=False).delete()
-        bot.delete_message(chat_id=chat_id, message_id=message_id)
-    except Exception as ex:
-        text = f"Function: auto_del_request\n" \
-               f"Xatolik yuz berdi: {ex.__str__()}"
-        bot.send_message(chat_id=settings.ADMIN_ID, text=text)
-
-
-inter_schedule, created = IntervalSchedule.objects.get_or_create(
-    every=12,
-    period=IntervalSchedule.HOURS,
-)
-
-
-def create_task(message):
-    periodic_task = PeriodicTask.objects.create(
-        interval=inter_schedule,
-        name=f'Send message {message}',
-        task='apps.tasks.send_message',
-        args=(message,),
-        one_off=True,
-    )
-    return periodic_task
-
-
-def create_auto_delete_req(message_id, chat_id, request_id):
-    periodic_task = PeriodicTask.objects.create(
-        interval=inter_schedule,
-        name=f'Auto delete request {request_id}',
-        task='apps.staff.tasks.auto_del_request',
-        args=json.dumps([message_id, chat_id, request_id]),
-        one_off=True,
-    )
-    return periodic_task
+# import json
+#
+# from celery import shared_task
+# from django_celery_beat.models import PeriodicTask, CrontabSchedule, IntervalSchedule
+# from telegram import Bot
+#
+# from apps.staff.models import Request_price
+# from config import settings
+#
+# bot = Bot(token=settings.S_TOKEN)
+#
+#
+# @shared_task
+# def send_message(message):
+#     bot.send_message(chat_id=settings.ADMIN_ID, text=message)
+#     return message
+#
+#
+# @shared_task
+# def auto_del_request(message_id, chat_id, request_id):
+#     try:
+#         Request_price.objects.filter(id=request_id, status=Request_price.Status.PENDING, answer=False).delete()
+#         bot.delete_message(chat_id=chat_id, message_id=message_id)
+#     except Exception as ex:
+#         text = f"Function: auto_del_request\n" \
+#                f"Xatolik yuz berdi: {ex.__str__()}"
+#         bot.send_message(chat_id=settings.ADMIN_ID, text=text)
+#
+#
+# inter_schedule, created = IntervalSchedule.objects.get_or_create(
+#     every=12,
+#     period=IntervalSchedule.HOURS,
+# )
+#
+#
+# def create_task(message):
+#     periodic_task = PeriodicTask.objects.create(
+#         interval=inter_schedule,
+#         name=f'Send message {message}',
+#         task='apps.tasks.send_message',
+#         args=(message,),
+#         one_off=True,
+#     )
+#     return periodic_task
+#
+#
+# def create_auto_delete_req(message_id, chat_id, request_id):
+#     periodic_task = PeriodicTask.objects.create(
+#         interval=inter_schedule,
+#         name=f'Auto delete request {request_id}',
+#         task='apps.staff.tasks.auto_del_request',
+#         args=json.dumps([message_id, chat_id, request_id]),
+#         one_off=True,
+#     )
+#     return periodic_task
--- apps/staff/utils.py
+++ apps/staff/utils.py
@@ -8,7 +8,7 @@ from dateutil.relativedelta import relativedelta
 from telegram import Bot, Update
 
 from apps.staff.models import *
-from apps.staff.tasks import create_auto_delete_req
+# from apps.staff.tasks import create_auto_delete_req
 from config.settings import S_TOKEN, URL_1C, LOGIN_1C, PASSWORD_1C
 from datetime import datetime, date
 
@@ -344,8 +344,8 @@ def applyAvans(update: Update, context: CallbackContext, worker_id=None):
             if staff.boss:
                 message = context.bot.send_message(chat_id=staff.boss.telegram_id, text=text, parse_mode="html",
                                                    reply_markup=acceptInlineButton(req.id))
-                create_auto_delete_req(message_id=message.message_id, chat_id=staff.boss.telegram_id,
-                                       request_id=req.id)  # Auto delete request
+                # create_auto_delete_req(message_id=message.message_id, chat_id=staff.boss.telegram_id,
+                #                        request_id=req.id)  # Auto delete request
                 update.message.reply_text(text=f"✅So`rov {staff.boss.full_name}ga yuborildi, ID:  {req.pk}")
             else:
                 try:
@@ -358,8 +358,8 @@ def applyAvans(update: Update, context: CallbackContext, worker_id=None):
                         accept_button = acceptInlineButton2(req.id)
                     message = context.bot.send_message(chat_id=boss.telegram_id, text=text, parse_mode="html",
                                                        reply_markup=accept_button)
-                    create_auto_delete_req(message_id=message.message_id, chat_id=boss.telegram_id,
-                                           request_id=req.id)  # Auto delete request
+                    # create_auto_delete_req(message_id=message.message_id, chat_id=boss.telegram_id,
+                    #                        request_id=req.id)  # Auto delete request
                     update.message.reply_text(f"✅So`rov bo`lim boshlig`iga yuborildi, ID: {req.id}")
                 except Exception as ex:
                     error = f"{obj}\n{ex.__str__()}"
