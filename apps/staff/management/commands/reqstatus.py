5fd28d88a9f6eb182720cdfcdb96dc16c78be4bb
rustamovjavohir <rustamovj366@gmail.com>
2023-09-07 14:58:51 +0500

    change reqstatus command

--- apps/staff/management/commands/reqstatus.py
+++ apps/staff/management/commands/reqstatus.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
 
     def updateRequestStatus(self):
         Request_price.objects.filter(answer=True).update(status=Request_price.Status.ACCEPTED)
+        Request_price.objects.filter(answer=False).update(status=Request_price.Status.PENDING)
 
     def handle(self, *args, **options):
         self.updateRequestStatus()
